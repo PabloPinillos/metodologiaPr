53e3b9610a1ee00ae091c759578b4641f808e448
pablo.pinillos09@gmail.com <pablo.pinillos09@gmail.com>
2021-03-29 16:05:30 +0200

    empezamos (traduccion uml)

--- src/VistaTerminal.java
+++ src/VistaTerminal.java
@@ -1,20 +1,67 @@
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.*;
 
 /**
- * 
+ *
  */
-public class VistaTerminal {
+public class VistaTerminal implements ControladorIO {
 
     /**
-     * Default constructor
+     *
+     */
+    private BufferedReader fromSystemBuffer;
+
+    /**
+     * Constructor
      */
     public VistaTerminal() {
+        fromSystemBuffer = new BufferedReader(new InputStreamReader(System.in));
     }
 
-    /**
-     * 
-     */
-    private BufferReader fromSystemBuffer;
+    @Override
+    public String leerEntrada() throws IOException {
+        String salida = fromSystemBuffer.readLine();
+        return salida;
+    }
 
+    @Override
+    public void pintarMenu() {
+
+    }
+
+    @Override
+    public String[] pedirLogin() throws IOException {
+        String[] login = new String[2];
+        System.out.println("Introduzca su usuario");
+        login[0] = fromSystemBuffer.readLine();
+        System.out.println("Introduzca su contraseña");
+        login[1] = fromSystemBuffer.readLine();
+        return login;
+    }
+
+    @Override
+    public List<String> pedirSingup() {
+        //POR QUE DEVUELVE LIST EN VEZ DE STRING[3]?
+        return null;
+    }
+
+    @Override
+    public void pintarOpcionesCliente() {
+        //TODO
+        System.out.println("Opciones cliente:");
+        System.out.println("1 - ...");
+        System.out.println("2 - ...");
+
+    }
+
+    @Override
+    public void pintarOpcionesAdministrador() {
+        //TODO
+        System.out.println("Opciones administrador:");
+        System.out.println("1 - ...");
+        System.out.println("2 - ...");
+    }
 }
