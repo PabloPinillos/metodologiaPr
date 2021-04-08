6d158d0a8c7ae0164586555237a0284acecfb037
Carlos <morillascarlos@gmail.com>
2021-04-08 17:42:36 +0200

    10

--- src/GestorCargueros.java
+++ src/GestorCargueros.java
@@ -6,40 +6,15 @@ import java.util.*;
  */
 public abstract class GestorCargueros extends IGestorNaves {
 
-    /**
-     * Default constructo
-     */
-    public GestorCargueros() {
-    }
-
-    /**
-     * @param String 
-     * @param Cliente 
-     * @param SistemaPropulsion[2] 
-     * @param int 
-     * @param SistemaDefensa[3] 
-     * @return
-     */
     public  Nave crearNave ( String  numeroRegistro , Cliente  propietario , SistemaPropulsion [] sistemasPropulsion , int  numeroTripulantes , SistemaDefensa [] sistemasDefensa ) {
-        return  new  EstacionEspacial (numeroRegistro, propietario, sistemasPropulsion, numeroTripulantes, sistemasDefensa, 0 );
+        return  new Carguero(numeroRegistro, propietario, sistemasPropulsion, numeroTripulantes, sistemasDefensa, 0 );
     }
 
-    /**
-     * @param Carguero 
-     * @param float
-     */
-    public void setCargaMaxima(void Carguero, void float) {
-        // TODO implement here
+    public void setCargaMaxima( Carguero carguero, float CargaMaxima ) {
+        Carguero.setCargaMaxima(CargaMaxima);
     }
 
-    /**
-     * @param String 
-     * @param Cliente 
-     * @param SistemaPropulsion[2] 
-     * @param int 
-     * @param SistemaDefensa[3] 
-     * @return
-     */
+
     public abstract Nave crearNave(void String, void Cliente, void SistemaPropulsion[2], void int, void SistemaDefensa[3]);
 
 }
