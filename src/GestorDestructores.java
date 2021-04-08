eb1dbcc0e0cb81598bc3b13512f57bc944fb8644
Carlos <morillascarlos@gmail.com>
2021-04-08 18:22:52 +0200

    10

--- src/GestorDestructores.java
+++ src/GestorDestructores.java
@@ -10,15 +10,15 @@ public abstract class GestorDestructores extends IGestorNaves {
         return new Destructor(numeroRegistro, propietario, sistemasPropulsion, numeroTripulantes, sistemasDefensa, 0 );
     }
 
-    public void setArmas(Destructor destructor, List of Arma Destructor) {
-        destructor.setArmas(Arma);
+    public void setArmas(Destructor destructor, List of Arma arma ) {
+        destructor.setArmas(arma);
     }
 
-    public void agregarArma(Destructor destructor,  Arma armas) {
-        destructor.agregarArma(Arma);
+    public void agregarArma(Destructor destructor,  Arma arma) {
+        destructor.agregarArma(arma);
     }
 
-    public void reemplazarArma(void Destructor, void int, void Arma) {
+    public void reemplazarArma(Destructor destructor , int , void Arma) {
         // TODO implement here
     }
 
