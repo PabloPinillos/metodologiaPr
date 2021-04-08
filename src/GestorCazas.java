c8170723585e53824894315bce1058ccb8178be5
Carlos <morillascarlos@gmail.com>
2021-04-08 17:35:11 +0200

    10

--- src/GestorCazas.java
+++ src/GestorCazas.java
@@ -6,49 +6,19 @@ import java.util.*;
  */
 public abstract class GestorCazas extends IGestorNaves {
 
-    /**
-     * Default constructor
-     */
-    public GestorCazas() {
-    }
-
-    /**
-     * @param String 
-     * @param Cliente 
-     * @param SistemaPropulsion[2] 
-     * @param int 
-     * @param SistemaDefensa[3] 
-     * @return
-     */
     public  Nave  crearNave ( String  numeroRegistro , Cliente  propietario , SistemaPropulsion [] sistemasPropulsion , int  numeroTripulantes , SistemaDefensa [] sistemasDefensa ) {
-        return  new  EstacionEspacial (numeroRegistro, propietario, sistemasPropulsion, numeroTripulantes, sistemasDefensa, 0 );
+        return  new  Caza (numeroRegistro, propietario, sistemasPropulsion, numeroTripulantes, sistemasDefensa, 0 );
     }
 
-    /**
-     * @param Caza 
-     * @param Arma[2]
-     */
-    public void setArmas(void Caza, void Arma[2]) {
-        // TODO implement here
+    public void setArmas(Caza caza, List of Arma Destructores) {
+        destructores.setArmas(Armas);
     }
 
-    /**
-     * @param Caza 
-     * @param int
-     * @param Arma
-     */
     public void reemplazarArma(void Caza, void int, void Arma) {
         // TODO implement here
     }
 
-    /**
-     * @param String 
-     * @param Cliente 
-     * @param SistemaPropulsion[2] 
-     * @param int 
-     * @param SistemaDefensa[3] 
-     * @return
-     */
+
     public abstract Nave crearNave(void String, void Cliente, void SistemaPropulsion[2], void int, void SistemaDefensa[3]);
 
 }
--- src/GestorDestructores.java
+++ src/GestorDestructores.java
@@ -7,15 +7,15 @@ import java.util.*;
 public abstract class GestorDestructores extends IGestorNaves {
 
     public Nave crearNave(String numeroRegistro, Cliente propietario, SistemaPropulsion[] sistemasPropulsion , int  numeroTripulantes , SistemaDefensa [] sistemasDefensa ) {
-        return new Destructores(numeroRegistro, propietario, sistemasPropulsion, numeroTripulantes, sistemasDefensa, 0 );
+        return new Destructor(numeroRegistro, propietario, sistemasPropulsion, numeroTripulantes, sistemasDefensa, 0 );
     }
 
-    public void setArmas(Destructores destructores, List of Arma Destructores) {
-        destructores.setArmas(Armas);
+    public void setArmas(Destructor destructor, List of Arma Destructor) {
+        destructor.setArmas(Arma);
     }
 
-    public void agregarArma(Destructores destructores,  Arma armas) {
-        destructores.agregarArma(Arma);
+    public void agregarArma(Destructor destructor,  Arma armas) {
+        destructor.agregarArma(Arma);
     }
 
     public void reemplazarArma(void Destructor, void int, void Arma) {
