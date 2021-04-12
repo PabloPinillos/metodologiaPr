e38cdefd80c9ee2efd06d505abe34163e422c2e8
Carlos <morillascarlos@gmail.com>
2021-04-13 01:45:43 +0200

    Implementacion valorarusuario y publicaroferta

--- src/LogicaSis.java
+++ src/LogicaSis.java
@@ -1,6 +1,6 @@
 
 import java.io.IOException;
-//hola
+//holaa
 /**
  *
  */
--- src/Sistema.java
+++ src/Sistema.java
@@ -2,7 +2,7 @@
 import java.io.IOException;
 import java.util.List;
 import java.util.ArrayList;
-//Hola
+//Holaa
 /**
  *
  */
