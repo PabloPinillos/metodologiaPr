4a242af5e3a0ec8217bb2d6db664a7c79cab9100
jaime <jaimemolinero12@gmail.com>
2021-04-13 16:52:27 +0200

    pintar cliente y arreglito de los switch en logicassis

--- src/LogicaSis.java
+++ src/LogicaSis.java
@@ -50,21 +50,21 @@ public class LogicaSis {
         opciones[3] = "C) Carguero";
         opciones[4] = "D) Estación espacial";
         IO.escribirTerminal(opciones);
-        opcionElegida = IO.leerEntrada();
+        opcionElegida = IO.leerEntrada().toUpperCase();
 
         String naveAux = null;
 
         switch (opcionElegida){
-            case "1":
+            case "D":
                 naveAux = "EstacionEspacial";
                 break;
-            case "2":
+            case "B":
                 naveAux = "Destructor";
                 break;
-            case "3":
+            case "A":
                 naveAux = "Caza";
                 break;
-            case "4":
+            case "C":
                 naveAux = "Carguero";
                 break;
             default:
@@ -85,21 +85,21 @@ public class LogicaSis {
         opciones[3] = "C) Carguero";
         opciones[4] = "D) Estación espacial";
         IO.escribirTerminal(opciones);
-        opcionElegida = IO.leerEntrada();
+        opcionElegida = IO.leerEntrada().toUpperCase();
 
         String naveAux = null;
 
         switch (opcionElegida){
-            case "1":
+            case "D":
                 naveAux = "EstacionEspacial";
                 break;
-            case "2":
+            case "B":
                 naveAux = "Destructor";
                 break;
-            case "3":
+            case "A":
                 naveAux = "Caza";
                 break;
-            case "4":
+            case "C":
                 naveAux = "Carguero";
                 break;
             default:
@@ -110,6 +110,16 @@ public class LogicaSis {
     }
 
     public void pintarOpcionesCliente(){
+        String[] opciones = new String[5];
+        opciones[0] = "Escoja una opción";
+        opciones[1] = "A) Buscar Oferta";
+        opciones[2] = "B) Mirar notificaciones";
+        opciones[3] = "C) Subir oferta";
+        opciones[4] = "D) Suscribirse a un tipo de nave";
+        opciones[5] = "E) Desuscribirse a un tipo de nave";
+        opciones[6] = "x) Cerrar sesión";
+
+        }
 
     }
 
