44e0eb762961dbf2c679192effd70f4aeb45aed0
pablo.pinillos09@gmail.com <pablo.pinillos09@gmail.com>
2021-04-16 18:47:12 +0200

    ficheros (y clases serializables)

---
+++ src/Ficherizador.java
@@ -0,0 +1,42 @@
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Ficherizador {
+
+    public Ficherizador() {
+    }
+
+    ;
+
+    /**
+     * @param fileName: nombre del fichero
+     * @param data:     lista de objetos a escribir
+     */
+    void escribirFichero(String fileName, List<Object> data) throws IOException {
+        ObjectOutputStream escribiendoFichero = new ObjectOutputStream(new FileOutputStream(fileName));
+        escribiendoFichero.writeObject(data);
+        escribiendoFichero.close();
+    }
+
+    /**
+     * @param fileName: nombre del fichero
+     * @return lista de objetos a recuperar
+     */
+    List<Object> leerFichero(String fileName) throws IOException, ClassNotFoundException {
+        try {
+            ObjectInputStream leyendoFichero = new ObjectInputStream(new FileInputStream(fileName));
+            List<Object> lista = (ArrayList<Object>) leyendoFichero.readObject();
+            leyendoFichero.close();
+            return lista;
+        } catch (FileNotFoundException e) {
+            File file = new File(fileName);
+            return new ArrayList<>();
+        }
+
+    }
+
+    ;
+
+
+}
